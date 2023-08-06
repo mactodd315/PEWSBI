b32458655a5577a6aacec601d6e96019cbc2d653
mactodd315 <mrtodd@syr.edu>
2023-08-06 13:31:20 -0400

    adding shell script to create inj.hdf as well as updated version of simulation file

--- code/simulate/simulate_data.py
+++ code/simulate/simulate_data.py
@@ -1,6 +1,7 @@
 from  pycbc.inject import InjectionSet
 from pycbc.types import TimeSeries, zeros
 import argparse, h5py
+import numpy as np
 
 parser = argparse.ArgumentParser(
                     prog = "Create Simulation Data: ",
@@ -11,29 +12,31 @@ parser.add_argument("injfile", type=str,
                    help="Path to the injection file, should be .hdf format.")
 parser.add_argument("output_file", type=str,
                     help = "Path for output file to be written, should be .hdf format.")
-parser.add_argument("-n", "--ninjections", action="store_const", const = '43')
 parser.add_argument("--signal-length", type=float, default = 4096)
 parser.add_argument("--delta-f", type=int, default = 1024)
+parser.add_argument("--epoch", type=float, default = 0)
+parser.add_argument("-v", "--verbose", action="store_true", default=False)
 
 
 args = parser.parse_args()
 
-print(args.delta_f)
-
 
 injfile = args.injfile
+if args.verbose:
+    print("Loaded injection file...")
 with h5py.File(injfile, 'r') as f1:
     injector = InjectionSet(injfile)
-    print(injector.table)
+    ninjections = len(injector.table)
 
-    with h5py.File(args.output_file, 'w') as f:
-        signal = np.zeros(0)
-        distances = twod_group.create_dataset("distances", data=distance_list)
-        inclinations = twod_group.create_dataset("inclinations", data=inclination_list)
-
-        for i in range(n_distance):
-            a = TimeSeries(zeros(4096), epoch=-1.0, delta_t=1.0/1024)
+    with h5py.File(args.output_file, 'w') as f:  
+        for i in f1.attrs['static_args']:
+            f['static_args/'+ i] = f1.attrs[i]
+        for i in f1.keys():
+            f['parameters/' + i] = f1[i][()]
+        signal = np.zeros((ninjections,args.signal_length))
+        for i in range(ninjections):
+            a = TimeSeries(zeros(args.signal_length), epoch=args.epoch, delta_t=1.0/args.delta_f)
             injector.apply(a, 'H1', simulation_ids=[i])
-            waveforms[i,:] = a
-            
+            signal[i,:] = a
+
         f['signals'] = signal
