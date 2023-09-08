c1d25fdf153297036d9c046afd635d4a765c53a9
mactodd315 <mrtodd@syr.edu>
2023-09-08 11:25:37 -0400

    updating locals

--- code/simulate/create_training_data.sh
+++ code/simulate/create_training_data.sh
@@ -4,12 +4,12 @@ source /home/mrtodd/miniconda3/bin/activate sbi_env
 
 bash /home/mrtodd/PEWSBI/code/simulate/create_injection.sh \
             /home/mrtodd/PEWSBI/code/simulate/injection1d.ini \
-            $2 \
-            /home/mrtodd/PEWSBI/datafiles/subfiles/local_injection$1.hdf
+            $1 \
+            /home/mrtodd/PEWSBI/datafiles/local_injection.hdf
 
-python  /home/mrtodd/PEWSBI/code/simulate/simulate_data.py \
-        /home/mrtodd/PEWSBI/datafiles/subfiles/local_injection$1.hdf \
-        /home/mrtodd/PEWSBI/datafiles/subfiles/local_simulation$1.hdf \
+python  /home/mrtodd/PEWSBI/bin/simulate_data.py \
+        /home/mrtodd/PEWSBI/datafiles/local_injection.hdf \
+        /home/mrtodd/PEWSBI/datafiles/local_simulation.hdf \
         --signal-length 1200 \
         --monitor-rate 40 \
         --pool-number 1 \
--- code/train/train.sh
+++ code/train/train.sh
@@ -2,13 +2,13 @@
 
 source /home/mrtodd/miniconda3/bin/activate sbi_env
 
-python /home/mrtodd/PEWSBI/code/train/train_neural_network.py \
-                           /home/mrtodd/PEWSBI/datafiles/cluster_simulation.hdf \
-                           /home/mrtodd/PEWSBI/neural_nets/neural_network_1d_$1.pickle \
-                           $1 \
-                           /home/mrtodd/PEWSBI/code/simulate/injection1d.ini \
-                           --noisefile /home/mrtodd/PEWSBI/code/simulate/noise.hdf \
-                           --add-noise \
-                           --monitor-rate 1000 \
-                           --verbose \
-                           --logfile /home/mrtodd/PEWSBI/logs/training_log.log
+python /home/mrtodd/PEWSBI/bin/train_neural_network.py \
+       /home/mrtodd/PEWSBI/datafiles/cluster_simulation.hdf \
+       /home/mrtodd/PEWSBI/neural_nets/neural_network_1d_$1.pickle \
+       $1 \
+       /home/mrtodd/PEWSBI/code/simulate/injection1d.ini \
+       --noisefile /home/mrtodd/PEWSBI/code/simulate/noise.hdf \
+       --add-noise \
+       --monitor-rate 1000 \
+       --verbose \
+       --logfile /home/mrtodd/PEWSBI/logs/cluster_training.log
