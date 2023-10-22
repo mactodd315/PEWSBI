2730386ef05c1225ccd162cc9b193324b837f5a6
mactodd315 <mrtodd@syr.edu>
2023-10-22 17:15:55 -0400

    updated code

--- bin/plot.py
+++ bin/plot.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(prog="Plotter"
 
 
 #------------------
-parser.add_argument("--posteriors-folder", required=True,
+parser.add_argument("--samples-folder", required=True,
                     help = "Path to file containing posteriors to be plotted.")
 parser.add_argument("--plot-folder", required=True)
 parser.add_argument("--plot-content", choices = ["pptest", "posterior"], required=True,
@@ -39,40 +39,38 @@ parser.add_argument("-v", "--verbose", action="store_true", default=False)
 args = parser.parse_args()
 
 ######################################################################
-def is_true(credibility, cumulative, parameter, bounds, bins):
-    theta = numpy.linspace(bounds[0],bounds[1],bins)
-    credibility_bounds = theta[numpy.searchsorted(cumulative,[0,credibility])]
-    if parameter<= credibility_bounds[1] and parameter >= credibility_bounds[0]:
+def is_true(credibility, dataset, parameter, bounds):
+    mid = len(dataset)//2
+    cutoff = int(credibility*len(dataset))//2
+    if parameter <= dataset[mid+cutoff] and parameter >= dataset[mid-cutoff]:
         return True
     else:
         return False
 #--------------------------------
 
-def run_pp_test(hdffile, key, args, s1, n_intervals = 21, **kwargs):
+def run_pp_test(hdffile, key, args, s1, n_intervals = 101, **kwargs):
     """This pp_test function initializes linearly spaced credible intervals, and then evaluates the cumulative distribution function to determine the "credibility" at that interval. It generates the plot WITHIN this function, so plt.show() must be called after the function is called.
     """
-    dataset = hdffile['posteriors/'+key][:argsd['num_posteriors'],:]
+    dataset = hdffile['samples/'+key][:argsd['num_posteriors']]
     parameters = hdffile['true_parameters/'+key][:argsd['num_posteriors']]
     bounds = hdffile['bounds/'+key][:]
 
     credible_intervals = numpy.linspace(0,1,n_intervals)
     trues_in_intervals = numpy.zeros(n_intervals)
-    cumulatives = [dataset[j,:].cumsum() for j in range(len(dataset))]
     for i in range(1,n_intervals-1):
         credibility = credible_intervals[i]
-        trues_list = [1 if is_true(credibility, cumulatives[j], parameters[j],
-                                    bounds,
-                                    bins=dataset.shape[1]) else 0 for j in range(len(dataset)) ]
-        trues_in_intervals[i] = sum(trues_list)/dataset.shape[0]
+        trues_list = [1 if is_true(credibility, numpy.sort(dataset[j]), parameters[j],
+                                    bounds) else 0 for j in range(len(dataset)) ]
+        trues_in_intervals[i] = sum(trues_list)/len(dataset)
     trues_in_intervals[-1] = 1
     
-    details = argsd['posterior_name'][10:-4]+'_'+key
+    
     if argsd['save_traces']:
-        trace_name = "traces"+details
+        trace_name = "traces"+key
         with h5py.File(argsd['parent_folder']+'/'+'plots/'+trace_name, 'w') as f:
             f['traces'] = (credible_intervals, trues_in_intervals)
-            f['label'] = details
-    s1.plot(credible_intervals, trues_in_intervals, label=details)
+            f['label'] = key
+    s1.plot(credible_intervals, trues_in_intervals, label=key)
     return s1
 
 
@@ -101,20 +99,24 @@ if __name__ == "__main__":
     argsd = vars(args)
     argsd["parent_folder"] = os.path.join(args.plot_folder,'..')
 
-    posteriors = [f for f in os.listdir(args.posteriors_folder) if f.split('.')[1]=='hdf']
+    samples = [f for f in os.listdir(args.samples_folder) if f.split('.')[1]=='hdf']
 
     fig, s1 = plt.subplots(1,1)
     if argsd['plot_content'] == "pptest":
-        for each in posteriors:
-            argsd['posterior_name'] = each
-            with h5py.File(args.posteriors_folder+'/'+each, 'r') as f:
+        for each in samples:
+            argsd['sample_name'] = each
+            with h5py.File(args.samples_folder+'/'+each, 'r') as f:
                 keys = argsd['sample_parameters'].split(',')
                 for key in keys:
                     s1 = run_pp_test(f, key, argsd, s1)
+        s1.set_title(f"PP Test for {argsd['sample_name']}")
+        s1.set_xlabel("Credible Interval")
+        s1.set_ylabel("Credibility")
+        s1.grid(True)
         plt.legend()
         plt.savefig(os.path.join(args.plot_folder,'pptest.png'))
     if args.plot_content == "posterior":
-        for each in posteriors:
+        for each in samples:
             argsd['posterior_name'] = each
             with h5py.File(os.path.join(args.posteriors_folder,each), 'r') as f:
                 plot_posterior(f,s1,argsd)
--- bin/sample.py
+++ bin/sample.py
@@ -3,7 +3,21 @@ import torch, numpy, pickle, h5py, argparse, sys, configparser, os
 
 sys.path.append("/home/mrtodd/PEWSBI/code/train")
 
-from train_neural_network import get_bounds_from_config
+def get_bounds_from_config(filepath):
+    cp = configparser.ConfigParser()
+    cp.read(filepath)
+    param_prior = [each for each in cp.sections() if each.split('-')[0]=='prior']
+    bounds = numpy.zeros((len(param_prior),2))
+
+    parameters = []
+    
+    for i in range(len(param_prior)):
+        each = param_prior[i]
+        parameter = each.split('-')[1]
+        parameters.append(parameter)
+        bounds[0, i] = float(cp[each]['min-'+parameter])
+        bounds[1, i] = float(cp[each]['max-'+parameter])
+    return bounds, parameters
 def details(neuralnet):
     dets = {}
     neuralnet = neuralnet.split('.')[0]
@@ -13,6 +27,14 @@ def details(neuralnet):
     dets['learning_rate'] = l[2][2:]
     dets['batch_size'] = l[3][2:]
     return dets
+def get_samples(net, observations, args):
+    samples = [net.sample((args.n_samples,), x=each) for each in observations]
+    
+    return samples
+def fetch_observation(file, args):
+    observations = torch.as_tensor(f['signals'][:args.observation_num,:])
+    true_parameters = [f['parameters'][key][:args.observation_num] for key in f['parameters'].keys()]
+    return observations, true_parameters
 
 
 if __name__ == "__main__":
@@ -32,8 +54,6 @@ if __name__ == "__main__":
                         help="Path to observation file (.hdf).")
     parser.add_argument("--config-file", type=str, required=True,
                         help="Path to config file (.ini).")
-    parser.add_argument("--sample-parameter", type=str, required=True,
-                        help="Parameter name to sample.")
     parser.add_argument("--observation-num", type=int, default=0)
     parser.add_argument("--n-samples", type=int, default=10000,
                          help="Number of samples to draw from neural network.")
@@ -47,29 +67,33 @@ if __name__ == "__main__":
     if not os.path.exists(args.output_folder):
         os.makedirs(args.output_folder)
 
+    bounds, parameters = get_bounds_from_config(args.config_file)
+    
     if args.verbose:
         print("Loading neural network...")
     with open(args.neural_net, 'rb') as f:
         net = pickle.load(f)
 
-    if args.verbose:
-        print("Fetching observations...")
+    if args.verbose:  print("Fetching observations...")
     with h5py.File(args.observation_file, 'r') as f:
-        observations = torch.as_tensor(f['signals'][:args.observation_num,:])
-        true_parameters = [f['parameters'][key][:args.observation_num] for key in f['parameters'].keys()]
-    if args.verbose:
-        print("Sampling...")
-    samples = [net.sample((args.n_samples,), x=each).cpu() for each in observations]
-    bounds = get_bounds_from_config(args.config_file, args.sample_parameter)
-    counts = [numpy.histogramdd(each, args.n_bins, range=(bounds[0],bounds[1]), density=True)[0] for each in samples]
-    posteriors = [each*(bounds[1]-bounds[0])/args.n_bins for each in counts]
+        observations, true_parameters = fetch_observation(f, args)
+    
+    
+    if args.verbose:  print("Sampling...")
+    samples = get_samples(net, observations, args)
+    samples = numpy.asanyarray(samples)
+
+    # getting data
     nn_params = details(args.neural_net)
-    posteriorname = f"posterior_TS{nn_params['training_number']}_LR{nn_params['learning_rate']}_BS{nn_params['batch_size']}.hdf"
-    with h5py.File(args.output_folder+'/'+posteriorname, 'w') as f:
-        f['posteriors'] = posteriors
-        f['posteriors'].attrs['training_size'] = nn_params['training_number']
-        f['true_parameters'] = true_parameters
-        if args.write_samples:
-            f['raw_samples'] = samples
-    if args.verbose:
-        print("All posterior estimates written.")
+    samplename = f"samples_TS{nn_params['training_number']}_LR{nn_params['learning_rate']}_BS{nn_params['batch_size']}.hdf"
+    
+    # writing posteriors
+    n_parameters = len(bounds[0])
+    with h5py.File(args.output_folder+'/'+samplename, 'w') as f:
+        for i in range(n_parameters):
+            
+            f['samples/'+parameters[i]] = samples[:,:,i]
+            f['bounds/'+parameters[i]] = [bounds[0][i], bounds[1][i]]
+            f['true_parameters/'+parameters[i]] = true_parameters[i]
+            
+    if args.verbose:  print("All samples written.")
