d1e78c21066b26d1453c46620286afbeb0488309
mactodd315 <mrtodd@syr.edu>
2024-01-30 11:25:27 -0500

    develped training example

--- examples/var_all_physical/code/train.sh
+++ examples/var_all_physical/code/train.sh
@@ -9,13 +9,13 @@ example_folder="/examples/var_all_physical"
 #train neural_network
 python $current_folder/bin/train_neural_network.py \
    --simulation-file $current_folder$example_folder/simulations/trainings_all_big.hdf \
-   --output-file $current_folder$example_folder/neural_nets/NN_all_snpe.pickle \
-   --n-simulations 300000 \
+   --output-file $current_folder$example_folder/neural_nets/NN_all_snpe_5e5.pickle \
+   --n-simulations 500000 \
    --training-parameters 'total_mass' 'mass_ratio'\
    --ini-file $current_folder$example_folder/code/injection.ini \
    --add-noise \
    --noise-file $current_folder$example_folder/simulations/noise.hdf \
    --batch-size 1000 \
-   --learning-rate 5e-4 \
+   --learning-rate 5e-5 \
    --show-summary \
    --verbose
