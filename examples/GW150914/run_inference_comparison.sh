baa01dfbbff3c76e2786850e9f76c950b9706b8e
mactodd315 <mrtodd@syr.edu>
2024-03-17 12:56:41 -0400

    updated inference comparison

--- examples/GW150914/run_inference_comparison.sh
+++ examples/GW150914/run_inference_comparison.sh
@@ -3,63 +3,63 @@ export PATH=$PATH:$PWD/../../bin
 
 
 ### Do marginalized time pycbc inference first ###
-# pycbc_create_injections --verbose \
-#     --config-files gw150914_injection.ini \
-#     --ninjections 1 \
-#     --seed 0 \
-#     --output-file gw150914_injection.hdf \
-#     --variable-params-section variable_params \
-#     --static-params-section static_params \
-#     --dist-section prior \
-#     --force
+pycbc_create_injections --verbose \
+    --config-files gw150914_injection.ini \
+    --ninjections 1 \
+    --seed 0 \
+    --output-file gw150914_injection.hdf \
+    --variable-params-section variable_params \
+    --static-params-section static_params \
+    --dist-section prior \
+    --force
 
-# pycbc_condition_strain \
-#     --fake-strain aLIGOZeroDetHighPower \
-#     --fake-strain-seed 1234 \
-#     --fake-strain-flow 10 \
-#     --sample-rate 2048 \
-#     --gps-start-time 1126259442 \
-#     --gps-end-time 1126259482 \
-#     --channel-name H1:SIMULATED_STRAIN \
-#     --injection-file gw150914_injection.hdf \
-#     --output-strain-file H-H1_SIMULATED_STRAIN-1126259442-40.gwf
+pycbc_condition_strain \
+    --fake-strain aLIGOZeroDetHighPower \
+    --fake-strain-seed 1234 \
+    --fake-strain-flow 10 \
+    --sample-rate 2048 \
+    --gps-start-time 1126259447 \
+    --gps-end-time 1126259477 \
+    --channel-name H1:SIMULATED_STRAIN \
+    --injection-file gw150914_injection.hdf \
+    --output-strain-file H-H1_SIMULATED_STRAIN-1126259447-30.gwf
     
-# pycbc_condition_strain \
-#     --fake-strain aLIGOZeroDetHighPower \
-#     --fake-strain-seed 1234 \
-#     --fake-strain-flow 10 \
-#     --sample-rate 2048 \
-#     --gps-start-time 1126259442 \
-#     --gps-end-time 1126259482 \
-#     --channel-name H1:SIMULATED_STRAIN \
-#     --injection-file gw150914_injection.hdf \
-#     --output-strain-file H-H1_SIMULATED_STRAIN-1126259442-40.hdf
+pycbc_condition_strain \
+    --fake-strain aLIGOZeroDetHighPower \
+    --fake-strain-seed 1234 \
+    --fake-strain-flow 10 \
+    --sample-rate 2048 \
+    --gps-start-time 1126259447 \
+    --gps-end-time 1126259477 \
+    --channel-name H1:SIMULATED_STRAIN \
+    --injection-file gw150914_injection.hdf \
+    --output-strain-file H-H1_SIMULATED_STRAIN-1126259447-30.hdf
 
-# OMP_NUM_THREADS=1 pycbc_inference \
-#     --config-file margtime.ini \
-#     --nprocesses 2 \
-#     --processing-scheme mkl \
-#     --output-file marg_150914.hdf \
-#     --seed 0 \
-#     --force \
-#     --verbose
+OMP_NUM_THREADS=1 pycbc_inference \
+    --config-file margtime.ini \
+    --nprocesses 2 \
+    --processing-scheme mkl \
+    --output-file marg_150914.hdf \
+    --seed 0 \
+    --force \
+    --verbose
 
-# # This reconstructs any marginalized parameters
-# OMP_NUM_THREADS=1 pycbc_inference_model_stats \
-#     --input-file marg_150914.hdf \
-#     --output-file demarg_150914.hdf \
-#     --nprocesses 1 \
-#     --reconstruct-parameters \
-#     --force \
-#     --verbose
+# This reconstructs any marginalized parameters
+OMP_NUM_THREADS=1 pycbc_inference_model_stats \
+    --input-file marg_150914.hdf \
+    --output-file demarg_150914.hdf \
+    --nprocesses 1 \
+    --reconstruct-parameters \
+    --force \
+    --verbose
 ###########################################################
 
 ###     Now run sbi_inference procedure      ###
 
-# generate injection parameters file
+generate injection parameters file
 pycbc_create_injections --verbose \
     --config-files margtime.ini \
-    --ninjections 100 \
+    --ninjections 100000 \
     --seed 0 \
     --output-file gw150914_training_injections.hdf \
     --variable-params-section variable_params \
@@ -72,10 +72,10 @@ pycbc_create_injections --verbose \
 # strain above.
 simulate_data \
     --verbose \
-    --signal-length 24576 \
+    --signal-length 61440 \
     --delta-f 2048 \
     --monitor-rate 500 \
-    --epoch 1126259442 \
+    --epoch 1126259447 \
     --DNRF '1e18' \
     --injfile gw150914_training_injections.hdf \
     --output-file gw150914_training_samples.hdf
@@ -90,19 +90,19 @@ train_neural_network \
  --training-parameters mass1 mass2 \
  --simulation-file gw150914_training_samples.hdf \
  --output-file gw150914_trained_nn.pickle \
- --n-simulations 100 \
+ --n-simulations 50000 \
  --add-noise \
  --noise-file noise.hdf \
  --DNRF '1e18' \
  --show-summary \
- --batch-size 100 \
+ --batch-size 500 \
  --learning-rate .00002 \
  -v
 
 sample \
     --neural-net gw150914_trained_nn.pickle \
     --output-file sbi_gw150914_samples.hdf \
-    --observation-file  H1:SIMULATED_STRAIN-1126259456-16.hdf \
+    --observation-file  H-H1_SIMULATED_STRAIN-1126259447-30.hdf \
     --strain-channel 'H1:SIMULATED_STRAIN' \
     --sample-parameters 'mass1' 'mass2' \
     --observation-num 1 \
