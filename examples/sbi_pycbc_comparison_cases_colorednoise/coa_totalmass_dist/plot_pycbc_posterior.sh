4ddcf32eb21aee0d15bfad30aaa126f959760a41
mactodd315 <mrtodd@syr.edu>
2024-07-03 09:35:52 -0400

    remove data

--- examples/sbi_pycbc_comparison_cases_colorednoise/coa_totalmass_dist/plot_pycbc_posterior.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-export PATH=$PATH:/home/mactodd315/Projects/machine_learning_sbi/PEWSBI/bin
-
-pycbc_inference_plot_posterior \
-    --input-file  marg_150914.hdf samples_pycbc.hdf \
-    --output-file sbi_gw150914_posterior.png \
-    --parameters  total_mass distance\
-    --expected-parameters total_mass:71 distance:410\
-    --plot-contours
--- examples/sbi_pycbc_comparison_cases_colorednoise/coa_totalmass_dist/run_pycbc_inference.sh
+++
@@ -1,8 +0,0 @@
-OMP_NUM_THREADS=1 pycbc_inference \
-    --config-file marg_time_injection.ini \
-    --nprocesses 2 \
-    --processing-scheme mkl \
-    --output-file marg_150914.hdf \
-    --seed 0 \
-    --force \
-    --verbose
