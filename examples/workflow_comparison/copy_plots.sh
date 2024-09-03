45957d0a8258fb2f1e795a7200afe1eb3fd1dca5
mactodd315 <mrtodd@syr.edu>
2024-09-03 15:45:48 -0400

    updating wf

--- examples/workflow_comparison/copy_plots.sh
+++ examples/workflow_comparison/copy_plots.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 cd $1
 
 public_html = /home/mrtodd/public_html/ml_pycbc_comparisons/$1
 
-if [ -e "$public_html" ]; 
-
-then cp /home/mrtodd/PEWSBI/examples/workflow_comparison/$1/*/*.png \
+if [ -e "$public_html" ]; then 
+    
+    cp /home/mrtodd/PEWSBI/examples/workflow_comparison/$1/*/*.png \
         -t $public_html
 
-else \
-    mkdir $public_html && \
+else 
+    mkdir $public_html
     cp /home/mrtodd/PEWSBI/examples/workflow_comparison/$1/*/*.png \
         -t $public_html
 
